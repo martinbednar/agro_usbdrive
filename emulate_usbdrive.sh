9f0b059239cb6a0e7d44ca3a9842d8dc5094d681
martinbednar <martin.bedn20@gmail.com>
2022-07-26 10:08:52 +0200

    Add Raspberry script for emulating USB drive.

---
+++ emulate_usbdrive.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd /sys/kernel/config/usb_gadget/
+mkdir -p usbdevice
+cd usbdevice
+echo 0x1d6b > idVendor # Linux Foundation
+echo 0x0104 > idProduct # Multifunction Composite Gadget
+echo 0x0100 > bcdDevice # v1.0.0
+echo 0x0200 > bcdUSB # USB2
+mkdir -p strings/0x409
+echo "fedcba9876543210" > strings/0x409/serialnumber
+echo "K2Mechatronics" > strings/0x409/manufacturer
+echo "K2M USB Drive" > strings/0x409/product
+mkdir -p configs/c.1/strings/0x409
+echo "Config 1: ECM network" > configs/c.1/strings/0x409/configuration
+echo 250 > configs/c.1/MaxPower
+# Add functions here
+FILE=/home/pi/usbdrive/usbdrive.img
+mkdir -p ${FILE/img/d}
+# mount -o loop,ro,offset=1048576 -t ext4 $FILE ${FILE/img/d} # FOR OLD WAY OF MAKING THE IMAGE
+mount -o loop,ro, -t vfat $FILE ${FILE/img/d} # FOR IMAGE CREATED WITH DD
+mkdir -p functions/mass_storage.usb0
+echo 1 > functions/mass_storage.usb0/stall
+echo 0 > functions/mass_storage.usb0/lun.0/cdrom
+echo 0 > functions/mass_storage.usb0/lun.0/ro
+echo 0 > functions/mass_storage.usb0/lun.0/nofua
+echo $FILE > functions/mass_storage.usb0/lun.0/file
+ln -s functions/mass_storage.usb0 configs/c.1/
+# End functions
+ls /sys/class/udc > UDC
